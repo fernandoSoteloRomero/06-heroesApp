8a1ee72a773f4c349fe06444aac7fc8ad7237680
Irvin Dominguez <irvinj.dominguez@outlook.com>
2024-04-23 14:51:21 -0600

    Login y register template done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
-//localhost:4200/heroes
 const routes: Routes = [
   {
     path: 'auth',
