f806766f3d15cd8066f583ee2f3d963b170ce583
Irvin Dominguez <irvinj.dominguez@outlook.com>
2024-04-24 11:54:50 -0600

    Buscar un heroe y utilizando el componente autocomplete

--- src/app/heroes/pages/search-hero-page/search-hero-page.component.ts
+++ src/app/heroes/pages/search-hero-page/search-hero-page.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-search-hero-page',
   templateUrl: './search-hero-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SearchHeroPageComponent {
+  constructor(private heroesService: HeroesService) {}
 
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService
+      .getSuggestions(value)
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent): void{
+
+    if(!event.option.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+
+    // esto nos serviria si tenemos un endpoint que solo trae un id o un nombre, etc.
+    // this.heroesService.getHeroById(hero.id)
+
+    this.selectedHero = hero;
+  }
 }
