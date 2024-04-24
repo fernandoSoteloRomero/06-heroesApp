d8bc31f3bbf6cf450bd889eddecc9dccc4f10da7
Irvin Dominguez <irvinj.dominguez@outlook.com>
2024-04-24 10:21:42 -0600

    Creamos el consumo del enpoint para obtener un solo heroe y manejamos el error

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { env } from '../../../env/env';
 
@@ -11,6 +11,16 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined>{
+
+
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError( error => of(undefined) )
+    )
+
+
   }
   
 }
